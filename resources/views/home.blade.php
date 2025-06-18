031b315820c85fc8c40e056a1a490b9437dd11e7
Blawness <razzorite@gmail.com>
2025-06-18 16:26:03 +0700

    update 1.0

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class(['dark' => ($appearance ?? 'system') == 'dark'])>
+    < <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+        <title>HOMEPAGE</title>
+
+
+    </head>
+    <body>
+        <h1> Halaman Home</h1>
+    </body>
+</html>
